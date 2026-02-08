d9034f1d176e0a2dd723b3090281cae8b84f4b73
lekhraj718chouhan <160110292+lekhraj718chouhan@users.noreply.github.com>
2026-02-08 17:12:55 +0530

    feat: enhance user authentication by implementing token generation, user registration with image upload, and login functionality

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,102 +1,93 @@
-import mongoose, { Schema } from "mongoose";
-import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
+import mongoose, {Schema} from "mongoose";
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
 
 const userSchema = new Schema(
     {
-        username: { 
-            type: String, 
-            required: true, 
-            unique: true, 
-            lowercase: true, 
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
             trim: true, 
-            index: true 
+            index: true
         },
-
-        email: { 
-            type: String, 
-            required: true, 
-            unique: true, 
-            lowercase: true, 
-            trim: true 
-        },
-
-        fullName: { 
-            type: String, 
-            required: true, 
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowecase: true,
             trim: true, 
-            index: true 
         },
-
-        avatar: { 
+        fullName: {
+            type: String,
+            required: true,
+            trim: true, 
+            index: true
+        },
+        avatar: {
             type: String, // cloudinary url
-            required: true 
+            required: true,
         },
-
-        coverImage: { 
-            type: String // cloudinary url
+        coverImage: {
+            type: String, // cloudinary url
         },
-
         watchHistory: [
-            { 
-                type: Schema.Types.ObjectId, 
-                ref: "Video" 
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Video"
             }
         ],
-
-        password: { 
-            type: String, 
-            required: [true, "Password is required"] 
+        password: {
+            type: String,
+            required: [true, 'Password is required']
         },
+        refreshToken: {
+            type: String
+        }
 
-        refreshToken: { 
-            type: String 
-        },
-    }, 
-    { 
-        timestamps: true 
+    },
+    {
+        timestamps: true
     }
-);
+)
 
-// Middleware: Hash password before saving
 userSchema.pre("save", async function (next) {
-    if (!this.isModified("password")) return next();
+    if(!this.isModified("password")) return next();
 
-    this.password = await bcrypt.hash(this.password, 10);
-    next();
-});
+    this.password = await bcrypt.hash(this.password, 10)
+    next()
+})
 
-// Instance Method: Check password
-userSchema.methods.isPasswordCorrect = async function (password) {
-    return await bcrypt.compare(password, this.password);
-};
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password)
+}
 
-// Instance Method: Access Token
-userSchema.methods.generateAccessToken = function () {
+userSchema.methods.generateAccessToken = function(){
     return jwt.sign(
-        { 
-            _id: this._id, 
-            email: this.email, 
-            username: this.username 
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName: this.fullName
         },
         process.env.ACCESS_TOKEN_SECRET,
-        { 
-            expiresIn: process.env.ACCESS_TOKEN_EXPIRY 
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
         }
-    );
-};
-
-// Instance Method: Refresh Token
-userSchema.methods.generateRefreshToken = function () {
+    )
+}
+userSchema.methods.generateRefreshToken = function(){
     return jwt.sign(
-        { 
-            _id: this._id 
-        }, 
-        process.env.REFRESH_TOKEN_SECRET, 
-        { 
-            expiresIn: process.env.REFRESH_TOKEN_EXPIRY 
+        {
+            _id: this._id,
+            
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
         }
-    );
-};
+    )
+}
 
-export const User = mongoose.model("User", userSchema);
+export const User = mongoose.model("User", userSchema)
